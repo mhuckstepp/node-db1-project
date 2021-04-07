08a73eacbec1bb86498f8cb98c802b8d1e20571a
Max Huckstepp <mhuckstepp@gmail.com>
2021-04-07 14:07:29 -0700

    mvp complete

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,30 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+  return db("accounts");
+  // return db.raw("select * from accounts limit 6");
+};
 
-const getById = id => {
-  // DO YOUR MAGIC
-}
+const getById = (id) => {
+  return db("accounts").first().where({ id });
+  // return db.raw(`select * from accounts where id = ${id}`);
+};
 
-const create = async account => {
-  // DO YOUR MAGIC
-}
+const create = async (account) => {
+  const [id] = await db("accounts").insert(account);
+  return getById(id);
+};
 
 const updateById = async (id, account) => {
-  // DO YOUR MAGIC
-}
+  await db("accounts").where({ id }).update(account);
+  return getById(id);
+};
 
-const deleteById = async id => {
-  // DO YOUR MAGIC
-}
+const deleteById = async (id) => {
+  const res = await getById(id);
+  await db("accounts").where({ id }).del();
+  return res;
+};
 
 module.exports = {
   getAll,
@@ -24,4 +32,4 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+};
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,31 +1,63 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const Accounts = require("./accounts-model");
 
-router.get('/', async (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get("/", async (req, res, next) => {
+  Accounts.getAll()
+    .then((accounts) => {
+      res.status(200).json(accounts);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/:id", (req, res, next) => {
+  Accounts.getById(req.params.id)
+    .then((account) => {
+      res.status(200).json(account);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.post("/", (req, res, next) => {
+  Accounts.create(req.body)
+    .then((account) => {
+      res.status(200).json(account);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.put("/:id", (req, res, next) => {
+  Accounts.updateById(req.params.id, req.body)
+    .then((account) => {
+      res.status(200).json(account);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete("/:id", (req, res, next) => {
+  Accounts.deleteById(req.params.id)
+    .then((account) => {
+      res.status(200).json(account);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// eslint-disable-next-line
+router.use((err, req, res, next) => {
   // CALL next(err) IF THE PROMISE REJECTS INSIDE YOUR ENDPOINTS
   res.status(500).json({
-    message: 'something went wrong inside the accounts router',
+    message: "something went wrong inside the accounts router",
     errMessage: err.message,
-  })
-})
+  });
+});
 
 module.exports = router;
